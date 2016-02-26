dc4071cc50c90323615221038f0ef76af3b34796
Doga Armangil <doga.armangil@alumni.epfl.ch>
2016-02-26 15:21:54 +0100

    add ruby version dependency to gem

--- lib/jsonapi_for_rails/version.rb
+++ lib/jsonapi_for_rails/version.rb
@@ -1,3 +1,5 @@
 module JsonapiForRails
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
+  # 0.1.2: add ruby version dependency to gem
+  # 0.1.1: fix @jsonapi_relationship[:params] bug, it is now set correctly
 end
